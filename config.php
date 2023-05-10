98acf66ef209968bac9a61de76fab95f1ca524a1
Valmir F <120138460+david-garcia1402@users.noreply.github.com>
2023-05-10 15:26:30 -0300

    Exclusão do bd

--- config.php
+++
@@ -1,8 +0,0 @@
-<?php
-//conexão com o banco de dados
-define('HOST', 'aws.connect.psdb.cloud');
-define('USER', 'di6pu0nlwekir01d6hvh');
-define('PASS', 'pscale_pw_RWSGsATXZECFtLH4xSiDiagPHvUApXKVrzAQZBO6Rl2');
-define('BASE', 'planetscale');
-
-$conn = new MySQLi(HOST, USER, PASS, BASE);
--- saveLike.php
+++
@@ -1,5 +0,0 @@
-<?php 
-include("config.php");
-$sql = "UPDATE NewTable SET likecount = likecount + 1 WHERE id_like = 1";
-$res = $conn->query($sql);
-?>
