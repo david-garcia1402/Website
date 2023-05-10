a90a87cf50892a1ca9363bee8df5bfc0f5ec30ec
Valmir F <120138460+david-garcia1402@users.noreply.github.com>
2023-05-10 15:18:09 -0300

    Update config.php

--- config.php
+++ config.php
@@ -1,8 +1,8 @@
 <?php
 //conexão com o banco de dados
 define('HOST', 'aws.connect.psdb.cloud');
-define('USER', '3g7awxkjdji8qfdi231r');
-define('PASS', 'pscale_pw_SwU69h9VyZXMycUL58mbzEMjQRXwcko2kB248l06I3V');
+define('USER', 'ykxedhp82upe4hvej0u9');
+define('PASS', 'pscale_pw_aFxSjW6qW4fAEj2nEPP2LFWiTEXHvZSpZY8eXHufFaT');
 define('BASE', 'planetscale');
 
 $conn = new MySQLi(HOST, USER, PASS, BASE);
